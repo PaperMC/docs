8bb56f158845b4f0b9d5859f33147143f7a49d34
Evan McCarthy <evanmccarthy@outlook.com>
2022-03-26 14:16:10 -0500

    Split and type config for easier configuration

--- config/sidebars.config.ts
+++ config/sidebars.config.ts
@@ -1,7 +1,6 @@
-// @ts-check
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 
-/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
-const sidebars = {
+const sidebars: SidebarsConfig = {
   docs: [
     {
       type: "link",
@@ -148,4 +147,4 @@ const sidebars = {
   ],
 };
 
-module.exports = sidebars;
+export default sidebars;

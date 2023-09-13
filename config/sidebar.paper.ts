fd4c25ec3537601e883f5161969f0fb270e8cc71
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2023-09-13 13:08:20 +0100

    Rework it to focus on "Internals" rather than NMS

--- config/sidebar.paper.ts
+++ config/sidebar.paper.ts
@@ -153,7 +153,7 @@ const paper: SidebarsConfig = {
             "dev/misc/reading-stacktraces",
             "dev/misc/debugging",
             "dev/misc/databases",
-            "dev/misc/nms",
+            "dev/misc/internal-code",
           ],
         },
       ],

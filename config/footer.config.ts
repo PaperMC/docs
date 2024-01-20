3c91ce0f8662ca7eb6754ca6d645e443abf0f1b3
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2024-01-20 20:04:33 +0000

    Move image to repo and decrease padding

--- config/footer.config.ts
+++ config/footer.config.ts
@@ -47,7 +47,7 @@ const footer: Footer = {
       ],
     },
   ],
-  copyright: `Copyright © ${new Date().getFullYear()} PaperMC and Contributors. Built with Docusaurus.<a href="https://vercel.com/?utm_source=papermc&utm_campaign=oss" style="text-decoration:underline;color:inherit;margin-top:15px;"><img src="https://images.ctfassets.net/e5382hct74si/78Olo8EZRdUlcDUFQvnzG7/fa4cdb6dc04c40fceac194134788a0e2/1618983297-powered-by-vercel.svg" alt="vercel"/></a>`,
+  copyright: `Copyright © ${new Date().getFullYear()} PaperMC and Contributors. Built with Docusaurus.<a href="https://vercel.com/?utm_source=papermc&utm_campaign=oss" style="text-decoration:underline;color:inherit;margin-top:10px;"><img src="/img/powered-by-vercel.svg" alt="vercel"/></a>`,
 };
 
 export default footer;

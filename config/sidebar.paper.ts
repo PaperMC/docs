ecc36d36cb7c7372c63bfc086a0690773fbd1b78
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2024-01-28 19:50:24 +0000

    Document Adventure Audiences (#290)

--- config/sidebar.paper.ts
+++ config/sidebar.paper.ts
@@ -127,11 +127,12 @@ const paper: SidebarsConfig = {
             },
             {
               type: "category",
-              label: "Component API",
+              label: "Component API (Adventure)",
               collapsed: true,
               items: [
                 "dev/api/component-api/intro",
                 "dev/api/component-api/i18n",
+                "dev/api/component-api/audiences",
               ]
             },
             "dev/api/how-do-plugins-work",

4147ce83a8124013fdbe61cda5734955cedc0c71
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2024-03-31 11:57:48 +0100

    A little silly tomfoolery (#334)

---
+++ src/theme/Navbar/ColorModeToggle/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useColorMode, useThemeConfig } from "@docusaurus/theme-common";
+import ColorModeToggle from "@theme/ColorModeToggle";
+import type { Props } from "@theme/Navbar/ColorModeToggle";
+import styles from "./styles.module.css";
+import ColorGenerator from "@site/src/april-fools/ColorGenerator";
+
+export default function NavbarColorModeToggle({ className }: Props): JSX.Element | null {
+  const navbarStyle = useThemeConfig().navbar.style;
+  const disabled = useThemeConfig().colorMode.disableSwitch;
+  const { colorMode, setColorMode } = useColorMode();
+
+  if (disabled) {
+    return null;
+  }
+
+  return (
+    <ColorModeToggle
+      className={className}
+      buttonClassName={navbarStyle === "dark" ? styles.darkNavbarColorModeToggle : undefined}
+      value={colorMode}
+      onChange={(value) => {
+        ColorGenerator.colorThemeChanged(value);
+        setColorMode(value);
+      }}
+    />
+  );
+}
---
+++ src/theme/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NavbarLayout from "@theme/Navbar/Layout";
+import NavbarContent from "@theme/Navbar/Content";
+import ColorGenerator from "@site/src/april-fools/ColorGenerator";
+
+export default function Navbar(): JSX.Element {
+  const foolsMode = typeof localStorage !== "undefined" ? localStorage.getItem("foolsMode") : false;
+
+  // Check to see if it is April Fools Day
+  const today = new Date();
+  const isAprilFools =
+    today >= new Date(today.getFullYear(), 3, 1) && today < new Date(today.getFullYear(), 3, 2);
+
+  if ((foolsMode === "true" || foolsMode === null) && isAprilFools) ColorGenerator.enable();
+
+  return (
+    <NavbarLayout>
+      <NavbarContent />
+    </NavbarLayout>
+  );
+}

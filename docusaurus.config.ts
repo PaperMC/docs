290cd55a811c0ce9413082551fca6562411d6c7e
powercas_gamer <cas@mizule.dev>
2024-06-13 21:49:20 +0000

    my dear friend prettier
    please stop harrasing me

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ import {
 } from "./src/util/authorUtils";
 
 // replace
-const preview = env.CF_PAGES_BRANCH !== "main"
+const preview = env.CF_PAGES_BRANCH !== "main";
 cacheAuthorData(preview || process.env.NODE_ENV === "development");
 
 const url = (preview && `https://${env.CF_PAGES_URL}`) || "https://docs.papermc.io";

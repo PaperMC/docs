548eae07bdf4421d7fee895616bb1363c106e684
Strokkur424 <133226102+Strokkur424@users.noreply.github.com>
2024-12-15 10:15:42 +0100

    Create command execution page

--- config/sidebar.paper.ts
+++ config/sidebar.paper.ts
@@ -136,6 +136,7 @@ const paper: SidebarsConfig = {
                 "dev/api/command-api/comparison",
                 "dev/api/command-api/command-tree",
                 "dev/api/command-api/arguments-and-literals",
+                "dev/api/command-api/command-execution",
               ],
             },
             {

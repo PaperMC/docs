14d2f8987725e6f82e8ba8053c429916d23bff22
Strokkur424 <133226102+Strokkur424@users.noreply.github.com>
2024-12-18 17:45:22 +0100

    Format

--- src/components/ui/FullWidthVideo.tsx
+++ src/components/ui/FullWidthVideo.tsx
@@ -1,8 +1,13 @@
 export default function FullWidthVideo({ src }: FullWidthVideoProps) {
-    return <div> <video width="100%" muted loop controls={true}>
-        <source src={src} type = "video/mp4"/>
+  return (
+    <div>
+      {" "}
+      <video width="100%" muted loop controls={true}>
+        <source src={src} type="video/mp4" />
         Your device does not support video playback
-    </video> </div>
+      </video>{" "}
+    </div>
+  );
 }
 
 interface FullWidthVideoProps {

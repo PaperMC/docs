f9c122b0aabb591b6badd88e8c8a00dcad3bd7ee
Strokkur24 <133226102+Strokkur424@users.noreply.github.com>
2024-12-21 17:04:33 +0100

    Migrate arguments to seperate pages

--- config/sidebar.paper.ts
+++ config/sidebar.paper.ts
@@ -135,30 +135,34 @@ const paper: SidebarsConfig = {
                 {
                   type: "category",
                   label: "Basics",
-                  collapsed: false,
+                  collapsed: true,
                   items: [
                     "dev/api/command-api/basics/introduction",
                     "dev/api/command-api/basics/command-tree",
+                    "dev/api/command-api/basics/arguments-and-literals",
                     "dev/api/command-api/basics/command-executors",
                     "dev/api/command-api/basics/command-registration",
-                  ]
+                  ],
                 },
                 {
                   type: "category",
                   label: "Arguments",
                   collapsed: true,
                   items: [
-                    "dev/api/command-api/arguments/arguments-and-literals",
-                    "dev/api/command-api/arguments/minecraft-arguments"
-                  ]
+                    "dev/api/command-api/arguments/minecraft-arguments",
+                    "dev/api/command-api/arguments/location-arguments",
+                    "dev/api/command-api/arguments/entity-player-arguments",
+                    "dev/api/command-api/arguments/registry-arguments",
+                    "dev/api/command-api/arguments/enum-value-arguments",
+                    "dev/api/command-api/arguments/predicate-arguments",
+                    "dev/api/command-api/arguments/adventure-arguments",
+                  ],
                 },
                 {
                   type: "category",
                   label: "Misc",
                   collapsed: true,
-                  items: [
-                    "dev/api/command-api/misc/comparison-bukkit-brigadier"
-                  ]
+                  items: ["dev/api/command-api/misc/comparison-bukkit-brigadier"],
                 },
               ],
             },

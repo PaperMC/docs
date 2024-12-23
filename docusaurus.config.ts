fb9da1943c588b99dbd4d8ecb6c90e07983632d9
MiniDigger | Martin <admin@minidigger.dev>
2024-12-23 18:31:10 +0100

    chore: fix format

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,7 +81,7 @@ const config: Config = {
         src: "https://trk.papermc.io/api/init",
         "data-website-id": "f93a0860-1253-4868-b27a-08208a36cea7",
       },
-    }
+    },
   ],
 
   markdown: {

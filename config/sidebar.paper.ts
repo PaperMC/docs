2949769b094a17987f21a75f639622d013e95111
Owen <23108066+Owen1212055@users.noreply.github.com>
2025-02-03 11:39:28 -0500

    feat: data components (#514)
    * Add Data Component Documentation
    
    * fix typos
    
    * fix: misc improvements (#535)
    
    * Cleanup data components page
    
    * Fixup small mistake
    
    * Fix minor spelling
    
    * Small changes
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Matouš Kučera <mk@kcra.me>
    
    ---------
    
    Co-authored-by: Strokkur24 <133226102+Strokkur424@users.noreply.github.com>
    Co-authored-by: Matouš Kučera <mk@kcra.me>

--- config/sidebar.paper.ts
+++ config/sidebar.paper.ts
@@ -179,7 +179,7 @@ const paper: SidebarsConfig = {
             },
             {
               type: "category",
-              label: "Component API (Adventure)",
+              label: "Text Component API (Adventure)",
               collapsed: true,
               items: [
                 "dev/api/component-api/intro",
@@ -187,6 +187,7 @@ const paper: SidebarsConfig = {
                 "dev/api/component-api/audiences",
               ],
             },
+            "dev/api/data-component-api",
             "dev/api/pdc",
             "dev/api/custom-inventory-holder",
             "dev/api/scheduler",

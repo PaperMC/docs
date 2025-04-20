0308f33c3a17e1a53c8adc6fa62d5ec83e35d216
Strokkur424 <133226102+Strokkur424@users.noreply.github.com>
2025-04-20 21:35:40 +0200

    pnpm format

--- src/utils/git-utils.ts
+++ src/utils/git-utils.ts
@@ -13,19 +13,13 @@ const headers = {
   },
 };
 
-const repo: string = "PaperMC/docs"
+const repo: string = "PaperMC/docs";
 const cache: Map<string, GitHubAccount> = new Map();
 
 // Git
-export async function getGitHubAccountFromFile(
-  filePath: string
-): Promise<GitHubAccount | null> {
-  const displayName = execSync(
-    `git log -1 --pretty="format:%an" -- "${filePath}"`
-  ).toString();
-  const email = execSync(
-    `git log -1 --pretty="format:%ae" -- "${filePath}"`
-  ).toString();
+export async function getGitHubAccountFromFile(filePath: string): Promise<GitHubAccount | null> {
+  const displayName = execSync(`git log -1 --pretty="format:%an" -- "${filePath}"`).toString();
+  const email = execSync(`git log -1 --pretty="format:%ae" -- "${filePath}"`).toString();
 
   const cached = cache.get(email);
   if (cached != null) {
@@ -46,17 +40,17 @@ export async function getGitHubAccountFromFile(
 
   // As the email seems to not be directly linked to an account, we instead use the GitHub API
   const url = new URL(`https://api.github.com/repos/${repo}/commits`);
-  url.searchParams.set('path', filePath);
-  url.searchParams.set('per_page', '1');
+  url.searchParams.set("path", filePath);
+  url.searchParams.set("per_page", "1");
 
   // It is **KNOWN** that if a commit is not pushed to GitHub, the link will be of the person who last edited the page remotely.
   // This is only a bug in local dev or other branch environments, and not in production.
-  const commit = (await fetch(url, headers).then(response => response.json()))[0]
+  const commit = (await fetch(url, headers).then((response) => response.json()))[0];
   let acc: GitHubAccount = {
     displayName: displayName,
     email: email,
-    accountLink: commit?.author?.html_url
-  }
+    accountLink: commit?.author?.html_url,
+  };
 
   cache.set(email, acc);
   return acc;
@@ -64,9 +58,7 @@ export async function getGitHubAccountFromFile(
 
 // E-Mail related
 
-export async function getGitHubAccountLinkByEmail(
-  email: string
-): Promise<string | null> {
+export async function getGitHubAccountLinkByEmail(email: string): Promise<string | null> {
   const url = new URL(`https://api.github.com/search/users?q=${email}`);
   const result = await fetch(url, headers).then((response) => response.json());
 

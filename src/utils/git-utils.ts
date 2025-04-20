d88d79ea419a929e18db2b0b56cf4d5f0aa9b571
Strokkur424 <133226102+Strokkur424@users.noreply.github.com>
2025-04-20 22:38:02 +0200

    Fix incorrect author link by retrieving commit has instead of latest commit

--- src/utils/git-utils.ts
+++ src/utils/git-utils.ts
@@ -25,33 +25,30 @@ const headers: RequestInit =
       };
 
 const repo: string = "PaperMC/docs";
-const cache: Map<string, GitHubAccount> = new Map();
+const emailCache: Map<string, GitHubAccount> = new Map();
 
 // Git
 export async function getGitHubAccountFromFile(filePath: string): Promise<GitHubAccount | null> {
-  const displayName = execSync(`git log -1 --pretty="format:%an" -- "${filePath}"`).toString();
   const email = execSync(`git log -1 --pretty="format:%ae" -- "${filePath}"`).toString();
-
-  const cached = cache.get(email);
+  const cached = emailCache.get(email);
   if (cached != null) {
-    cached.displayName = displayName;
     return cached;
   }
 
+  const displayName = execSync(`git log -1 --pretty="format:%an" -- "${filePath}"`).toString();
+  const hash = execSync(`git log -1 --pretty="format:%H" -- "${filePath}"`).toString();
+
   // As the email seems to not be directly linked to an account, we instead use the GitHub API
-  const url = new URL(`https://api.github.com/repos/${repo}/commits`);
-  url.searchParams.set("path", filePath);
+  const url = new URL(`https://api.github.com/repos/${repo}/commits/${hash}`);
   url.searchParams.set("per_page", "1");
 
-  // It is **KNOWN** that if a commit is not pushed to GitHub, the link will be of the person who last edited the page remotely.
-  // This is only a bug in local dev or other branch environments, and not in production.
-  const commit = (await fetch(url, headers).then((response) => response.json()))[0];
+  let commit = await fetch(url, headers).then((response) => response.json());
   let acc: GitHubAccount = {
-    displayName: displayName,
+    displayName: commit.commit?.author?.name ?? displayName,
     email: email,
     accountLink: commit?.author?.html_url,
   };
 
-  cache.set(email, acc);
+  emailCache.set(email, acc);
   return acc;
 }

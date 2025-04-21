8caacd24fe1967e582a39dec495d4e4371f9a908
Strokkur24 <133226102+Strokkur424@users.noreply.github.com>
2025-04-21 14:33:50 +0200

    feat: add last update commit+committer (#575)
    * Add last author of page
    
    * Remove debug logging and add dev note
    
    * pnpm format
    
    * Use GitHub token if possible and remove redundant API call
    
    * Make time zone be dependant on client's time zone
    
    * Undo client side time zone because scorp just told me that it runs server side anyways meaning that it would not even work regardless
    
    * Fix incorrect author link by retrieving commit has instead of latest commit
    
    * Add Discord server invite link
    
    * refactor: error handling, add commit link
    
    * fix: pass in GitHub token from workflow
    
    * Undo tag styling
    
    * fix: wrap
    
    * fix: format
    
    ---------
    
    Co-authored-by: Matouš Kučera <mk@kcra.me>
    Co-authored-by: Pedro <3602279+Doc94@users.noreply.github.com>

---
+++ src/utils/git.ts
@@ -0,0 +1,64 @@
+import { execSync } from "child_process";
+
+export interface CommitterInfo {
+  name: string;
+  href: string;
+}
+
+export interface CommitInfo {
+  hash: string;
+  committer: CommitterInfo;
+}
+
+const token = process.env.GITHUB_TOKEN;
+
+const options: RequestInit = token
+  ? {
+      headers: {
+        Accept: "application/vnd.github+json",
+        "User-Agent": "papermc-docs/author",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  : {
+      headers: {
+        Accept: "application/vnd.github+json",
+        "User-Agent": "papermc-docs/author",
+      },
+    };
+
+export const REPO = "PaperMC/docs";
+const cache = new Map<string, CommitterInfo>();
+
+export const getCommitInfo = async (filePath: string): Promise<CommitInfo | null> => {
+  let email: string, hash: string;
+  try {
+    email = execSync(`git log -1 --pretty="format:%ae" -- "${filePath}"`).toString();
+    hash = execSync(`git log -1 --pretty="format:%H" -- "${filePath}"`).toString();
+  } catch (e) {
+    return null;
+  }
+
+  const cached = cache.get(email);
+  if (cached) {
+    return { hash, committer: cached };
+  }
+
+  let name: string;
+  try {
+    name = execSync(`git log -1 --pretty="format:%an" -- "${filePath}"`).toString();
+  } catch (e) {
+    return null;
+  }
+
+  const info: CommitterInfo = { name, href: `mailto:${email}` };
+
+  const res = await fetch(`https://api.github.com/repos/${REPO}/commits/${hash}`, options);
+  if (res.ok) {
+    const commit = await res.json();
+    info.href = commit.author.html_url;
+  }
+
+  cache.set(email, info);
+  return { hash, committer: info };
+};

59d749ba38f8ab5c3ee80b893a2f321397290c7e
Privatech <99482159+Privatech38@users.noreply.github.com>
2025-08-02 17:56:33 +0200

    Run prettier on NoteParticleColorPicker.svelte

--- src/components/tools/NoteParticleColorPicker.svelte
+++ src/components/tools/NoteParticleColorPicker.svelte
@@ -1,7 +1,10 @@
 <script lang="ts" module>
-
   let offsetX: number = $state(0.0);
-  let color = $derived(`rgb(${noteColorRGB(offsetX).map(c => c * 100 + "%").join(', ')})`);
+  let color = $derived(
+    `rgb(${noteColorRGB(offsetX)
+      .map((c) => c * 100 + "%")
+      .join(", ")})`
+  );
 
   /**
    * Calculates RGB color based on the offsetX value, just like the Minecraft client.
@@ -12,18 +15,15 @@
     return [
       Math.max(0.0, Math.sin(offsetX * 6.2831855) * 0.65 + 0.35),
       Math.max(0.0, Math.sin((offsetX + 0.33333334) * 6.2831855) * 0.65 + 0.35),
-      Math.max(0.0, Math.sin((offsetX + 0.6666667) * 6.2831855) * 0.65 + 0.35)
+      Math.max(0.0, Math.sin((offsetX + 0.6666667) * 6.2831855) * 0.65 + 0.35),
     ];
   }
-
 </script>
 
 <div class="picker">
-
   <div class="colorBox" style="background-color: {color};"></div>
   <p><span class="value">offsetX = {offsetX}</span></p>
   <input class="offset-slider" type="range" min={-1.0} max={1.0} step={0.01} bind:value={offsetX} />
-
 </div>
 
 <style>
